3c7463339b4e78ca35c01d101152810b34a097b3
rusrushal13 <rusrushal13@gmail.com>
2019-05-28 15:10:32 +0530

    added whitenoise for serving static files

--- eyequant_api_project/settings/base.py
+++ eyequant_api_project/settings/base.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'eyequant_api_project.urls'
@@ -130,6 +131,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
